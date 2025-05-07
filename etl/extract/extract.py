3588f9424eb09aadfb2f3c9066e366b57673e647
Jules James <jules.james594@gmail.com>
2025-05-07 13:35:31 +0100

    added test

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import requests
 
-def extract_api(starttime,endttime):
+
+def extract_api(starttime, endttime):
     try:
         query = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={starttime}&endtime={endttime}'
         data = requests.get(query)
-        df = pd.json_normalize(data.json(),'features')
+        df = pd.json_normalize(data.json(), 'features')
         return df
 
     except Exception as e:
-        print(f'unable to retrieve data: ')
+        print(f'unable to retrieve data: {e}')
