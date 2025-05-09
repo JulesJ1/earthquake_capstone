35da400563fe32382ce6545f99b37a8b546bde46
JulesJ1 <jules.james594@gmail.com>
2025-05-09 22:37:05 +0100

    added more error handling

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -2,12 +2,27 @@ import pandas as pd
 import requests
 
 
+class BadRequestError(Exception):
+    pass
+
+
+class RequestTimeoutError(Exception):
+    pass
+
+
+class GeneralException(Exception):
+    pass
+
 def extract_api(starttime, endttime):
     try:
         query = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={starttime}&endtime={endttime}'
-        data = requests.get(query)
+        data = requests.get(query, timeout=5)
+        data.raise_for_status()
         df = pd.json_normalize(data.json(), 'features')
         return df
-
+    except requests.exceptions.HTTPError as e:
+        raise BadRequestError(f'HTTP error: {e}')
+    except requests.exceptions.ReadTimeout as e:
+        raise RequestTimeoutError(f'Timed out: {e}')
     except Exception as e:
-        print(f'unable to retrieve data: {e}')
+        raise GeneralException(f'unable to retrieve data: {e}')
