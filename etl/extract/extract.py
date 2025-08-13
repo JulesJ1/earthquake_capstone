7dd530464163fbce66413a197c49a0708838f9fd
JulesJ1 <jules.james594@gmail.com>
2025-08-13 23:44:14 +0100

    cleanup

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -25,8 +25,6 @@ logger = setup_logger(__name__, "database.log", level=logging.DEBUG)
 
 def extract_api(query):
     try:
-        # query = 'https://earthquake.usgs.gov/fdsnws/event/1/query'\
-        #    f'?format=geojson&starttime={starttime}&endtime={endttime}'
 
         data = requests.get(query, timeout=60)
         data.raise_for_status()
